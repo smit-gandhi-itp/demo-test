2689ef491feb87b2a5b1b3f768fd70da2c550a5c
smit-gandhi-itp <smit.gandhi@intuitive.ai>
2026-05-21 22:47:32 +0530

    feat: add hello_world.py script

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Hello World Python Script
+
+A simple Python script that prints 'Hello, World!' to the console.
+"""
+
+def main():
+    """Main function that prints Hello, World!"""
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    main()
